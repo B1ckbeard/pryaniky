11033a327f9d72fa0bc481cab41b2c39e377e4cb
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2024-05-20 20:52:41 +0300

    deploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import routes from './routes';
